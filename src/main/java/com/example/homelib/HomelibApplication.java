7f029e013f086d0305695ff4328baa60f14323cc
Daniel S <slota.daniel@gmail.com>
2020-10-14 18:52:51 +0200

    Initial commit

---
+++ src/main/java/com/example/homelib/HomelibApplication.java
@@ -0,0 +1,13 @@
+package com.example.homelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomelibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homelib/HomelibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homelib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomelibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
